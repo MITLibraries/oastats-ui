7592c00f22c3fbb4dde422a6002c78c9627d330f
Matt Bernhardt <mjbernha@mit.edu>
2013-12-13 16:44:35 -0500

    Changing form container ID to avoid a conflict; Adding listbuilder plugin
    Signed-off-by: Matt Bernhardt <mjbernha@mit.edu>

--- includes/form_filter.php
+++ includes/form_filter.php
@@ -1,13 +1,5 @@
-<div id="filter">
+<div id="oafilter">
 <?php
-
-if ($reqD!="") {
-	echo "<p>DLC List Builder</p>";
-} else if ($reqA!="") {
-	echo "<p>Author List Builder</p>";
-} else {
-	echo "<p>General List Builder</p>";
-}
 
 $cursor = $collection->aggregate(
 	array('$group' => array( '_id'=>'$dlc')),
@@ -16,15 +8,16 @@ $cursor = $collection->aggregate(
 
 ?>
 	<form method="get">
-	<select id="filter" name="filter" multiple="true">
-	<?php
-		foreach($cursor["result"] as $document) {
-			echo "<option value=\"".$document["_id"]."\">".$document["_id"]."</option>";
-		}
-	?>
-	</select>
-	<a href="#" id="test">Click</a>
+		<label for="filter">
+			Show only these departments:<br>
+			<select id="filter" name="filter" multiple="true" class="listbuilder">
+			<?php
+				foreach($cursor["result"] as $document) {
+					echo "<option value=\"".$document["_id"]."\">".$document["_id"]."</option>";
+				}
+			?>
+			</select>
+		</label>
 	<input type="submit">
 	</form>
 </div>
-<script type="text/javascript" src="/scripts/list-builder.js"></script>
--- index.php
+++ index.php
@@ -21,9 +21,29 @@ if(isset($_GET["a"])) {
 		<title>OA Statistics Mockup</title>
 		<link rel="stylesheet" href="styles/reset.css">
 		<link rel="stylesheet" href="styles/styles.css">
+		<link rel="stylesheet" href="styles/listbuilder.css">
 		<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
 		<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
 		<script src="http://d3js.org/d3.v3.min.js" charset="utf-8"></script>
+		<script src="scripts/listbuilder.js" charset="utf-8"></script>
+		<script>
+$(document).ready(function() {
+
+	var listbuilder = app.listbuilder;
+	listbuilder.initialize(
+		debugFlag = false
+	);
+
+	$('.listcontainer .option').click(function() {
+		// Stop whatever might otherwise happen
+		// event.preventDefault();
+		// Pass the value of the clicked item to listbuilder
+		var target = $(this);
+		listbuilder.getClick(target);
+	});
+
+});	
+		</script>		
 	</head>
 	<body>
 		<div class="container">
@@ -39,6 +59,7 @@ if(isset($_GET["a"])) {
 			<section class="main">
 				<h1>Open Access Statistics Mockup</h1>
 				<?php require_once('includes/form_filter.php'); ?>
+				<p>Hello, World!</p>
 			</section>
 			<?php require_once('includes/include_footer.html'); ?>
 		</div>
