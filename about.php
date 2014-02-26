929e188e6f28473020d35e07fdcf95aed44e5ab3
Matt Bernhardt <mjbernha@mit.edu>
2014-02-26 13:22:24 -0500

    Removing the about page
    Signed-off-by: Matt Bernhardt <mjbernha@mit.edu>

--- about.php
+++
@@ -1,5 +0,0 @@
-<?php 
-	require_once('includes/header.php'); 
-?>
-<p>This is the about page.</p>
-<?php require_once('includes/footer.php'); ?>
--- includes/include_login.php
+++ includes/include_login.php
@@ -7,7 +7,6 @@
 				<div id="navigation">
 					<ul>
 						<li><a href="index.php">Home</a></li>
-						<li><a href="about.php">About</a></li>
 <?php
 	if(isset($_SESSION["user"])) {
 ?>
