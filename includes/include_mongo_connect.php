47edb233a0653a9e4bf22e697c5bf9bb0332b1e9
Matt Bernhardt <mjbernha@mit.edu>
2014-02-27 16:20:13 -0500

    Reworking connection string to allow different environments to use their own db/collection names
    Signed-off-by: Matt Bernhardt <mjbernha@mit.edu>

--- includes/include_mongo_connect.php
+++ includes/include_mongo_connect.php
@@ -4,20 +4,28 @@
 		switch ($_SERVER["SERVER_NAME"]) {
 			case "oastats-dev.mit.edu":
 				$m = new Mongo('mongodb://libdb-dev.mit.edu:27017');
+				$db = $m->oatest;
+				$collection = $db->request;
+				$summaries = $db->summaries;
 				break;
 			case "oastats-test.mit.edu":
 				$m = new Mongo('mongodb://libdb-test.mit.edu:27017');
+				$db = $m->oatest;
+				$collection = $db->request;
+				$summaries = $db->summaries;
 				break;
 			case "oastats.mit.edu":
 				$m = new Mongo('mongodb://libdb-test.mit.edu:27017');
+				$db = $m->oatest;
+				$collection = $db->request;
+				$summaries = $db->summaries;
 				break;
 			default:			
 				$m = new Mongo();
+				$db = $m->oastats;
+				$collection = $db->requests;
+				$summaries = $db->summaries;
 		}
-		// $m = new Mongo();
-		$db = $m->oatest;
-		$collection = $db->request;
-		$summaries = $db->summaries;
 	} catch (Exception $e) {
 		die('Error: ' . $e->getMessage());
 	} catch (MongoConnectionException $e) {
