3eb22455c6c1cbc1ba7ab8b8d10e4605ad41f085
Matt Bernhardt <mjbernha@mit.edu>
2014-03-04 15:31:15 -0500

    Adding unsetters in destroyer for other session variables; Hiding salt value
    Signed-off-by: Matt Bernhardt <mjbernha@mit.edu>

--- destroyer.php
+++ destroyer.php
@@ -2,6 +2,9 @@
 	session_start();
 	
 	unset($_SESSION["user"]);
+	unset($_SESSION["admin"]);
+	unset($_SESSION["impersonate"]);
+	unset($_SESSION["hash"]);
 
 	$strBaseURL = 'https://'.$_SERVER["SERVER_NAME"].'/';
 
