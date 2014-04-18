8b12d9a4b5b6646501a133fb6e23603378eda825
Matt Bernhardt <mjbernha@mit.edu>
2014-04-18 10:25:48 -0400

    Adding callback to conditionally hide datatable pagination if only one page is visible. This closes #42
    Signed-off-by: Matt Bernhardt <mjbernha@mit.edu>

--- tab_data.php
+++ tab_data.php
@@ -16,13 +16,20 @@ session_start();
 			"bLengthChange": false,
 			"bInfo": false,
 			"sPaginationType": "full_numbers",
-			"iDisplayLength": 25
+			"iDisplayLength": 25,
+			"fnDrawCallback": function() {
+		        var pagination = this.attr('id')+'_paginate';
+				if(this.fnSettings().fnRecordsDisplay()<=25){
+					$("#"+pagination).hide();
+				} else {
+					$("#"+pagination).show();
+				}
+			}
 		});
 
 		var toggle = $(".paging_full_numbers").append('<a class="showall paginate_button">Show All</a>');
 
 		$(".showall").click(function() {
-			console.log("clicked");
 			var dtSettings = dt.fnSettings();
 			var label = $(this).html();
 			if(label == "Show All") {
@@ -33,7 +40,6 @@ session_start();
 				$(this).text("Show All");
 			}
 			dt.fnDraw();
-			console.log("changed");
 		});
 
 		// Set export options
